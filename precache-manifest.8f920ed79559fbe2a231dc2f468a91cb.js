df3cea333dd8cc9b09b729956fdebf39bd2a075d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-03 08:53:07 +0000

    Deploy kinder9898/tariffs to github.com/kinder9898/tariffs.git:gh-pages

---
+++ precache-manifest.8f920ed79559fbe2a231dc2f468a91cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd7436c925fa8cacb70febd5eb43fcb",
+    "url": "/tariffs/index.html"
+  },
+  {
+    "revision": "62d53e24c55a5dfc68e2",
+    "url": "/tariffs/static/css/main.1c19b7a4.chunk.css"
+  },
+  {
+    "revision": "b5bc3d37c73adb2943c0",
+    "url": "/tariffs/static/js/2.c643a220.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tariffs/static/js/2.c643a220.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62d53e24c55a5dfc68e2",
+    "url": "/tariffs/static/js/main.13a42c4c.chunk.js"
+  },
+  {
+    "revision": "01eaa79565fcc1bd8fd5",
+    "url": "/tariffs/static/js/runtime-main.b6e0952f.js"
+  }
+]);
